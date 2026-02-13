7210714fab82a2cef0fff7b8f0680c0b6f22277e
sirikancharla12 <sirikancharla1290@gmail.com>
2026-02-13 20:14:40 +0530

    add file support

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,29 +1,92 @@
-import { Plus } from 'lucide-react';
-import { useRef } from 'react';
+import { Check, Plus } from 'lucide-react';
+import { useRef, useState } from 'react';
 
 const Form = () => {
+
+  const [uploaded, setuploaded] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [folders, setFolders] = useState([]);
+  const [title, setTitle] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
+  const [folderFiles, setFolderFiles] = useState([]);
+
 
   const fileInputRef = useRef(null);
   const folderInputRef = useRef(null);
 
   const handleFileClick = () => {
-    console.log("File picked");
     fileInputRef.current.click();
-  }
+  };
 
   const handleFolderClick = () => {
     folderInputRef.current.click();
-  }
+  };
 
   const handleFileChange = (e) => {
-    const files = e.target.files;
+    const selectedFiles = Array.from(e.target.files);
 
-    for (const file of files) {
-      console.log(file.name);
+    setuploaded(true);
+    setFiles(prev => [...prev, ...selectedFiles]);
+
+    if (selectedFiles.length === 1) {
+      setTitle(selectedFiles[0].name);
+    } else {
+      setTitle(`${selectedFiles.length} files`);
     }
+
+    selectedFiles.forEach(f => console.log(f.name));
 
     e.target.value = null;
   };
+
+  const handleFolderChange = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    if (!selectedFiles.length) return;
+
+    const folderName =
+      selectedFiles[0].webkitRelativePath.split("/")[0];
+
+    setuploaded(true);
+
+    setFolderFiles(prev => [...prev, ...selectedFiles]);
+
+    setFolders(prev => [
+      ...prev,
+      {
+        name: folderName,
+        count: selectedFiles.length,
+        size: selectedFiles.reduce((a, f) => a + f.size, 0)
+      }
+    ]);
+
+    setTitle(folderName);
+
+    e.target.value = null;
+  };
+
+
+
+  const formatSize = (bytes) => {
+    if (bytes === 0) return "0 B";
+    const k = 1024;
+    const sizes = ["B", "KB", "MB", "GB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toFixed(1) + " " + sizes[i];
+  };
+
+  const fileExtension = (filename) => {
+    const extension = filename.split('.').pop();
+    return extension;
+  }
+
+  const fileCount = files.length;
+  const folderCount = folders.length;
+
+  const totalItems = fileCount + folderCount;
+
+
+
+
 
   return (
     <div className="min-h-screen bg-neutral-900 flex items-center justify-center ">
@@ -31,11 +94,11 @@ const Form = () => {
       <div className="bg-white w-[360px] rounded-2xl p-4 shadow-lg">
 
         <input type="file" multiple className='hidden' ref={fileInputRef} onChange={handleFileChange} />
-        <input type="file" webkitdirectory="true" directory="" className='hidden' ref={folderInputRef} onChange={handleFileChange} />
+        <input type="file" webkitdirectory="" directory="" className='hidden' ref={folderInputRef} onChange={handleFolderChange} />
 
         <form className="flex flex-col gap-4 text-sm">
 
-          <div className="grid grid-cols-2 gap-1">
+          {!uploaded && (<div className="grid grid-cols-2 gap-1">
             <div className=" bg-[#e0eaff] p-4 justify-center items-center flex rounded-xl">
 
               <div className='flex flex-col items-center gap-2 h-15'>
@@ -49,7 +112,6 @@ const Form = () => {
                   add files
                 </div>
               </div>
-
             </div>
             <div className="bg-[#e0eaff] p-4 justify-center items-center flex  rounded-xl">
 
@@ -66,6 +128,92 @@ const Form = () => {
               </div>
             </div>
           </div>
+          )
+          }
+
+          {
+            uploaded && (
+              <div className=''>
+                {files.map((f, i) => (
+                  <div key={i} className="text-black text-sm bg-[#f1f1f1] p-2 rounded-lg mb-1 ">
+                    {f.name}
+                    <p className='text-gray-500'>{formatSize(f.size)}  </p>
+                  </div>
+
+                ))}
+
+                {folders.map((f, i) => (
+                  <div key={"folder" + i}
+                    className="text-black text-sm bg-[#f1f1f1] p-2 rounded-lg mb-1">
+                    {f.name}
+                    <p className="text-gray-500">
+                      Folder {f.count} item
+                    </p>
+                  </div>
+                ))}
+
+
+                <div className='bg-[#e0eaff] p-2 rounded-lg mb-1 flex items-center justify-between'>
+                  <div className='flex gap-3 items-center justify-between '>
+                    <div className='text-blue-500'>
+                      <Check />
+                    </div>
+                    <div className='text-blue-500 font-medium'>
+                      {totalItems} {totalItems > 1 ? "items" : "item"} added
+                    </div>
+
+                  </div>
+
+                  <div className="relative flex items-center">
+
+                    <div
+                      className="text-blue-500 font-semibold text-sm cursor-pointer"
+                      onClick={() => setShowMenu(prev => !prev)}
+                    >
+                      Add more
+                    </div>
+
+                    <button
+                      className="bg-blue-500 rounded-full p-2 text-white ml-2"
+                      type="button"
+                      onClick={() => setShowMenu(prev => !prev)}
+                    >
+                      <Plus size={10} />
+                    </button>
+
+                    {showMenu && (
+                      <div className="absolute right-0 top-8 bg-white shadow-lg rounded-xl  text-sm overflow-hidden z-50">
+
+                        <div
+                          className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                          onClick={() => {
+                            handleFileClick();
+                            setShowMenu(false);
+                          }}
+                        >
+                          Files
+                        </div>
+
+                        <div
+                          className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                          onClick={() => {
+                            handleFolderClick();
+                            setShowMenu(false);
+                          }}
+                        >
+                          Folders
+                        </div>
+
+                      </div>
+                    )}
+                  </div>
+
+                </div>
+              </div>
+
+
+            )
+          }
 
           <div className="flex flex-col border-b border-gray-300 pb-2">
             <label className="text-gray-500">Your email</label>
@@ -80,6 +228,8 @@ const Form = () => {
             <label className="text-gray-500">Title</label>
             <input
               type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               className="bg-transparent outline-none text-gray-800"
             />
           </div>
